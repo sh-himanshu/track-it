582cdf69511db0f2e4f603e551efec3eb755857b
Himanshu Sharma <himanshu.sh1220@gmail.com>
2023-10-17 19:02:53 +0530

    refactor(root): integrate shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { DropdownMenuDemo } from '@/components/DropdownMenuDemo';
+
 const Home = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div className="flex flex-col items-center  justify-center">
+      <DropdownMenuDemo />
+    </div>
+  );
 };
 
 export default Home;
